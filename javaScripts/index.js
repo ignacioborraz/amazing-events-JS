11fb823eae9eb60f4284664301ce6a7d43ab56a5
ignacioborraz <ignacioborraz@hotmail.com>
2022-10-13 00:25:41 -0300

    no module

--- javaScripts/index.js
+++ javaScripts/index.js
@@ -27,8 +27,8 @@ let innerOptions = (category) => document.querySelector("#defaultList").innerHTM
 
 let printEvents = (array) => {
     document.querySelector("#events").innerHTML = ""
-    array.forEach(event =>{ //realizo un mapeo para configurar la impresión de los eventos futuros/pasados
-        document.querySelector("#events").innerHTML += // acumulo los eventos
+    array.forEach(event =>{
+        document.querySelector("#events").innerHTML +=
             `
             <a href="evento.html?id=${event.id}" class="d-flex m-2 imgWidth hoverEvent">
             <article class="d-flex flex-column justify-content-center align-items-center imgWidth">
@@ -53,61 +53,3 @@ let filter = (array,obj) => {
     }
     printEvents(filterArray)
 }
-
-
-/* 
-
-
-
-var parameter = "" // variable que va a contener el valor del select
-var data = [] // array que va a contener los eventos a imprimir
-var valueOfInput = "" // variable que va a contener el valor del input
-
-function toSelect (event) { // funcion que captura el valor proveniente del select e imprime el array de los eventos correspondientes
-    parameter = event.target.value // defino el valor del evento
-    document.querySelector("#events").innerHTML = "" // limpio la impresion de HTML
-    if (parameter == "" || parameter == "CATEGORIAS" || parameter == undefined) {
-        if (valueOfInput == "" || valueOfInput == undefined) {
-            data = futureEvents
-        } else {
-            data = futureEvents.filter(event =>
-                event.category.toLowerCase().startsWith(valueOfInput.toLowerCase()) ||
-                event.name.toLowerCase().startsWith(valueOfInput.toLowerCase())
-            )
-        }
-    } else {
-        if (valueOfInput == "" || valueOfInput == undefined) {
-            data = futureEvents.filter(event => event.category == parameter)
-        } else {
-            data = futureEvents.filter(event =>
-                event.category == parameter &&
-                (event.category.toLowerCase().startsWith(valueOfInput.toLowerCase()) ||
-                event.name.toLowerCase().startsWith(valueOfInput.toLowerCase()))
-            )
-        }
-    }
-    toPrint(data) // imprimimos en html
-    return parameter
-}
-
-function toSearch (event) { // funcion que captura el valor proveniente del input e imprime el array de los eventos correspondientes
-    valueOfInput = event.target.value // defino el valor del evento
-    document.querySelector("#events").innerHTML = "" // limpio la impresion de HTML
-    if (parameter == "" || parameter == undefined || parameter == "CATEGORIAS") {
-        data = futureEvents.filter(event =>
-            event.category.toLowerCase().startsWith(valueOfInput.toLowerCase()) ||
-            event.name.toLowerCase().startsWith(valueOfInput.toLowerCase())
-        )
-    } else {
-        data = futureEvents.filter(event =>
-            event.category.toLowerCase() === parameter.toLowerCase() &&
-            (event.category.toLowerCase().startsWith(valueOfInput.toLowerCase()) ||
-            event.name.toLowerCase().startsWith(valueOfInput.toLowerCase()))
-        )
-    }
-    toPrint(data) // imprimimos en html
-    return valueOfInput
-}
-
-
-toPrint(futureEvents) // imprimo en pantalla */
